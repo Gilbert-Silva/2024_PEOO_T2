d8416ed6d81971fbd9b0b935c41108cf2fbd31ea
Gilbert-Silva <gilbertif1995@outlook.com>
2024-08-27 19:02:03 +0000

    Exercícios

--- Projeto/cliente.py
+++ Projeto/cliente.py
@@ -12,6 +12,11 @@ class Cliente:
     self.nome = nome
     self.email = email
     self.fone = fone
+  def get_nome(self):
+    return self.__nome
+  def set_nome(self, nome):
+    if nome != "": self.nome = nome
+    else: raise ValueError("Nome inválido")  
   def __str__(self):
     return f"{self.id} - {self.nome} - {self.email} - {self.fone}"
 
@@ -41,9 +46,10 @@ class Clientes:
   def atualizar(cls, obj):
     c = cls.listar_id(obj.id)
     if c != None:
-       c.nome = obj.nome
-       c.email = obj.email
-       c.fone = obj.fone
+      c.nome = obj.nome
+      #c.set_nome( obj.get_nome() )
+      c.email = obj.email
+      c.fone = obj.fone
     cls.salvar()   
   @classmethod
   def excluir(cls, obj):
@@ -54,7 +60,7 @@ class Clientes:
   @classmethod
   def salvar(cls):  
     with open("clientes.json", mode = "w") as arquivo:   # write
-        json.dump(cls.objetos, arquivo, default = vars) 
+      json.dump(cls.objetos, arquivo, default = vars) 
   @classmethod
   def abrir(cls):
     cls.objetos = []
@@ -67,58 +73,3 @@ class Clientes:
     except FileNotFoundError:
       pass
     
-# Visão
-class UI:
-  @staticmethod
-  def menu():
-    print("1 - Inserir cliente, 2 - listar clientes, 3 - atualizar cliente, 4 - excluir cliente, 9 - fim")
-    return int(input("Informe uma opção: "))
-
-  @staticmethod
-  def main():
-    op = 0
-    while op != 9: 
-      op = UI.menu()
-      if op == 1: UI.cliente_inserir()
-      if op == 2: UI.cliente_listar()
-      if op == 3: UI.cliente_atualizar()
-      if op == 4: UI.cliente_excluir()
-
-  @staticmethod
-  def cliente_inserir():
-    #id = int(input("Informe o id: "))
-    nome = input("Informe o nome: ")
-    email = input("Informe o e-mail: ")
-    fone = input("Informe o fone: ")
-    c = Cliente(0, nome, email, fone)
-    Clientes.inserir(c)
-
-  @staticmethod
-  def cliente_listar():
-    for c in Clientes.listar():
-      print(c)
-
-  @staticmethod
-  def cliente_atualizar():
-    UI.cliente_listar()
-    id = int(input("Informe o id do cliente a ser atualizado: "))
-    nome = input("Informe o novo nome: ")
-    email = input("Informe o novo e-mail: ")
-    fone = input("Informe o novo fone: ")
-    c = Cliente(id, nome, email, fone)
-    Clientes.atualizar(c)
-
-  @staticmethod
-  def cliente_excluir():
-    UI.cliente_listar()
-    id = int(input("Informe o id do cliente a ser excluído: "))
-    c = Cliente(id, "", "", "")
-    Clientes.excluir(c)
-
-UI.main()    
-
-
-
-
-
-  
