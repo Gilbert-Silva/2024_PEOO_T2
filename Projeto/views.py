9deb8c80b9cd01d8826b95e02b00daa9dd132195
Gilbert-Silva <gilbertif1995@outlook.com>
2024-11-01 16:12:48 -0300

    Lista08

--- Projeto/views.py
+++ Projeto/views.py
@@ -1,14 +1,18 @@
 from models.cliente import Cliente, Clientes
 from models.horario import Horario, Horarios
 from models.servico import Servico, Servicos
+from datetime import datetime, timedelta
 
 class View:
-    def cliente_inserir(nome, email, fone):
-        c = Cliente(0, nome, email, fone)
+    def cliente_inserir(nome, email, fone, senha):
+        c = Cliente(0, nome, email, fone, senha)
         Clientes.inserir(c)
 
     def cliente_listar():
         return Clientes.listar()    
+
+    def cliente_listar_id(id):
+        return Clientes.listar_id(id)    
 
     def cliente_atualizar(id, nome, email, fone):
         c = Cliente(id, nome, email, fone)
@@ -29,7 +33,7 @@ class View:
         return Horarios.listar()    
 
     def horario_atualizar(id, data, confirmado, id_cliente, id_servico):
-        c = Horario(0, data)
+        c = Horario(id, data)
         c.confirmado = confirmado
         c.id_cliente = id_cliente
         c.id_servico = id_servico
@@ -39,12 +43,43 @@ class View:
         c = Horario(id, None)
         Horarios.excluir(c)    
 
+    def horario_abrir_agenda(data, hora_inicio, hora_fim, intervalo):
+        #data = "30/10/2024"
+        #inicio = "08:00"
+        #fim = "12:00"
+        #intervalo = 30
+        i = data + " " + hora_inicio   # "30/10/2024 08:00"
+        f = data + " " + hora_fim      # "30/10/2024 12:00"
+        d = timedelta(minutes=intervalo)
+        di = datetime.strptime(i, "%d/%m/%Y %H:%M")
+        df = datetime.strptime(f, "%d/%m/%Y %H:%M")
+        x = di
+        while x <= df:
+            #cadastrar o horário x
+            View.horario_inserir(x, False, None, None)
+            #passar para o próximo horário
+            x = x + d
+        """
+        h = "30/10/2024 08:00"
+        h = "30/10/2024 08:30"
+        h = "30/10/2024 09:00"
+        h = "30/10/2024 09:30"
+        h = "30/10/2024 10:00"
+        h = "30/10/2024 10:30"
+        h = "30/10/2024 11:00"
+        h = "30/10/2024 11:30"
+        h = "30/10/2024 12:00"
+        """
+
     def servico_inserir(descricao, valor, duracao):
         c = Servico(0, descricao, valor, duracao)
         Servicos.inserir(c)
 
     def servico_listar():
         return Servicos.listar()    
+
+    def servico_listar_id(id):
+        return Servicos.listar_id(id)    
 
     def servico_atualizar(id, descricao, valor, duracao):
         c = Servico(id, descricao, valor, duracao)
